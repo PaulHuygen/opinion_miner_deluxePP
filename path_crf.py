af7c0139c5d889482a3adaec935109ce2b9eab3f
root <PaulHuygen>
2019-02-27 12:11:07 +0000

    Thisdir works for symbolic links too

--- path_crf.py
+++ path_crf.py
@@ -1,1 +1,1 @@
-PATH_TO_CRF_TEST='/home/paul/temp/opinion_miner_deluxePP/crf_lib/CRF++-0.58/crf_test'
+PATH_TO_CRF_TEST='/usr/local/share/pipelines/nlpp/nlppmodules/opinion_miner_deluxePP/crf_lib/CRF++-0.58/crf_test'
